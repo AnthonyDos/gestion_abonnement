57b7bd25ca10e2f8d7da252cb1a542e3521bf4d9
AnthonyDos <dossantosanthony060@gmail.com>
2025-01-08 11:40:06 +0100

    ajout du cors

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const connectDB = require('./db/connection.js');
 const utilisateurRoutes = require('./routes/utilisateurs.js');
 const abonnementRoutes = require('./routes/abonnements.js');
+const cors = require('cors');  
 
 require('dotenv').config();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+}));
+
 app.use(express.json());
 
 // Connexion à la base de données
@@ -15,10 +21,6 @@ connectDB();
 app.use('/api/utilisateurs', utilisateurRoutes);
 app.use('/api/abonnements', abonnementRoutes);
 
-// Route de test
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
 // Démarrage du serveur
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
