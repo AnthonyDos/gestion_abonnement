6d711ab417c060cf552580aa0bf73a7222ea2db1
AnthonyDos <dossantosanthony060@gmail.com>
2025-01-09 18:12:32 +0100

    ajout des headers

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
-const connectDB = () => {
-  return mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connexion à MongoDB réussie'))
-    .catch((error) => console.error('Erreur de connexion à MongoDB:', error));
+// const connectDB = () => {
+//   return mongoose.connect(process.env.MONGO_URI)
+//     .then(() => console.log('Connexion à MongoDB réussie'))
+//     .catch((error) => console.error('Erreur de connexion à MongoDB:', error));
+// };
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connexion à MongoDB réussie');
+  } catch (error) {
+    console.error('Erreur de connexion à MongoDB:', error);
+  }
 };
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -8,9 +8,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-}));
+// Configurer CORS avec express
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); 
+  next();
+});
 
 app.use(express.json());
 
